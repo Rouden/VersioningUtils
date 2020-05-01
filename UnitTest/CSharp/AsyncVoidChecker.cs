a319f058c389ba0da82f508d33b5646239cf3123
Rouden <Rouden@users.noreply.github.com>
2020-05-01 19:30:58 +0900

    UseAsyncVoid で許容する関数名を指定できるようにした

--- UnitTest/CSharp/AsyncVoidChecker.cs
+++ UnitTest/CSharp/AsyncVoidChecker.cs
@@ -23,13 +23,18 @@ namespace XUnitPattern
         [Fact]
         public async Task UseAsyncVoid()
         {
+            // async void を許容する関数
+            string[] extFunctions = new string[] {
+                "OnInspectorGUI", // Unity エディタ拡張
+            };
+
             var files = await Utils.GetVersionedFiles(new string[] { ".cs" });
-            var reg = new Regex(@"\s+async\s+void\s+");
+            var reg = new Regex($@"\s+async\s+void\s+(?!{String.Join('|', extFunctions)})");
             var failedList = new List<string>();
             foreach (var path in files)
             {
                 // 例外
-                //if (path.Contains("something")) continue;
+                if (path.EndsWith("AsyncVoidChecker.cs")) continue; // このファイル
 
                 var text = File.ReadAllText(path, Encoding.UTF8);
                 if (reg.IsMatch(text))
@@ -37,7 +42,7 @@ namespace XUnitPattern
                     failedList.Add(path);
                 }
             }
-            Assert.True(0 == failedList.Count(), $"async 関数が void を返す関数が見つかりました。戻り値を Task または Task<T> に変更してください。\n{String.Join('\n', failedList)}");
+            Assert.True(0 == failedList.Count(), $"void を返す async 関数が見つかりました。戻り値を Task または Task<T> に変更してください。\n{String.Join('\n', failedList)}");
         }
     }
 }
